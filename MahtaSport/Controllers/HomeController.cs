39383552572987ae38e101d2cc2ef02a77c6367d
mahdieh <74619945+MahdiehTari@users.noreply.github.com>
2023-09-10 21:36:13 +0330

    add new model for index page

--- MahtaSport/Controllers/HomeController.cs
+++ MahtaSport/Controllers/HomeController.cs
@@ -8,15 +8,54 @@ public class HomeController : Controller
 {
     private readonly ILogger<HomeController> _logger;
     private string? contact;
+    private readonly List<kindofyoo> _kind = new List<kindofyoo>
+    {
+        new kindofyoo
+        {
+            image="sssss",
+            name = "e1",
+            text= "lorm oposum"
 
-    public HomeController(ILogger<HomeController> logger)
+        },
+        new kindofyoo
+        {
+            image="sssss",
+            name = "e2",
+            text= "lorm oposum"
+
+        },
+        new kindofyoo
+        {
+            image="sssss",
+            name = "e3",
+            text= "lorm oposum"
+
+        },
+        new kindofyoo
+        {
+            image="sssss",
+            name = "e4",
+            text= "lorm oposum"
+
+        }
+    };
+
+ public HomeController(ILogger<HomeController> logger)
     {
         _logger = logger;
     }
 
     public IActionResult Index()
     {
-        return View();
+        var cantact = new Contact()
+       
+        {
+            Maile = "mahdieh.tarikhchi@gmail.com",
+            Mobile = "09381248141",
+            Address = "mashhad"
+        };
+        
+        return View(_kind);
     }
 
     public IActionResult Contact()
---
+++ MahtaSport/Models/kindofyoo.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace MahtaSport.Models
+{
+	public class kindofyoo
+	{
+		public object name { get; set; }
+		public object image { get; set; }
+		public object text{ get; set; }
+	}
+}
+
